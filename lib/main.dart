b638dedf843acf6f584be8234bb6a69bbf15387a
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2025-01-30 23:02:09 +0300

    call widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      scrollBehavior: ScrollBehavior(),
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
--- lib/web_page.dart
+++ lib/web_page.dart
@@ -1,10 +1,11 @@
+import 'dart:ffi';
+
 import 'package:extended_sliver/extended_sliver.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:io';
 import 'dart:ui';
-
-
 
 class WebPage extends StatefulWidget {
   WebPage({Key? key}) : super(key: key);
@@ -14,156 +15,154 @@ class WebPage extends StatefulWidget {
 }
 
 class _NestedWebviewDemoState extends State<WebPage> {
-
-
   final ScrollController scrollController = ScrollController();
   late WebViewController webViewController;
   ValueNotifier<double> scrollHeightNotifier = ValueNotifier<double>(1);
-  ValueNotifier<int> progressNotifier = ValueNotifier<int>(0);
 
   @override
   void initState() {
     super.initState();
     webViewController = WebViewController()
       ..setNavigationDelegate(NavigationDelegate(
-
-          onPageStarted: (url) {
-          },
+          onPageStarted: (url) {},
           onPageFinished: (a) {
             print('Позиция ${scrollController.position}');
             webViewController.runJavaScript(scrollHeightJs);
-
           },
           onWebResourceError: (e) {
             //webViewStatusNotifier.value = WebViewStatus.failed;
             print('ERROR: ${e.errorCode}');
           },
-          onProgress: (progress) {
-            progressNotifier.value = progress;
-          }
-      ))..setJavaScriptMode(JavaScriptMode.unrestricted)
+          onProgress: (progress) {}))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..addJavaScriptChannel('ScrollHeightNotifier',
-          onMessageReceived: (message){
-            final String msg = message.message;
-            final double? height = double.tryParse(msg);
-            if (height != null) {
-              scrollHeightNotifier.value = height;
-            }
-          })
-      ..loadRequest(Uri.parse('https://flutter.dev'));
+          onMessageReceived: (message) {
+        final String msg = message.message;
+        final double? height = double.tryParse(msg);
+        if (height != null) {
+          scrollHeightNotifier.value = height;
+        }
+      })
+      ..loadRequest(Uri.parse('https://kdrc.ru'));
   }
+
   @override
   Widget build(BuildContext context) {
-
-    return  WillPopScope(
-      onWillPop: ()async{
-        if(await webViewController.canGoBack()){
+    return WillPopScope(
+      onWillPop: () async {
+        if (await webViewController.canGoBack()) {
           webViewController.goBack();
+          return false;
+        } else {
+          return true;
         }
-        return false;
       },
       child: Scaffold(
-          appBar: AppBar(
-            title: const Text('NestedWebview'),
-            actions: <Widget>[
-              TextButton(
-                  onPressed: () {
-                    scrollController.animateTo(
-                      144,
-                      duration: const Duration(seconds: 1),
-                      curve: Curves.linear,
+        // appBar: AppBar(
+        //   title: const Text('NestedWebview'),
+        //   actions: <Widget>[
+        //     TextButton(
+        //         onPressed: () {
+        //           scrollController.animateTo(
+        //             144,
+        //             duration: const Duration(seconds: 1),
+        //             curve: Curves.linear,
+        //           );
+        //         },
+        //         child: const Text(
+        //           'animate to Webview bottom',
+        //           style: TextStyle(
+        //             color: Colors.white,
+        //           ),
+        //         ))
+        //   ],
+        // ),
+
+        body: Stack(
+          children: <Widget>[
+            CustomScrollView(
+              controller: scrollController,
+              slivers: <Widget>[
+                SliverAppBar(
+                  title: Text('aaa'),
+                  expandedHeight: 200,
+                  floating: false,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    collapseMode: CollapseMode.pin,
+                    background: Image.asset(
+                      'assets/images/titleimage.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                ValueListenableBuilder<double>(
+                  valueListenable: scrollHeightNotifier,
+                  builder: (
+                    BuildContext context,
+                    double scrollHeight,
+                    Widget? child,
+                  ) {
+                    return SliverToNestedScrollBoxAdapter(
+                      childExtent: scrollHeight,
+                      onScrollOffsetChanged: (double scrollOffset) {
+                        double y = scrollOffset;
+                        if (Platform.isAndroid) {
+                          // https://github.com/flutter/flutter/issues/75841
+                          y *= window.devicePixelRatio;
+                        }
+                        webViewController.scrollTo(0, y.ceil());
+                      },
+                      child: child,
                     );
                   },
-                  child: const Text(
-                    'animate to Webview bottom',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ))
-            ],
-          ),
-          body: Stack(
-            children: <Widget>[
-              CustomScrollView(
-                //physics: NeverScrollableScrollPhysics(),
-                controller: scrollController,
-                slivers: <Widget>[
-                  // SliverToBoxAdapter(
-                  //   child: Container(
-                  //     height: 100,
-                  //     color: Colors.red,
-                  //     child: const Center(
-                  //       child: Text(
-                  //         'Header',
-                  //         style: TextStyle(color: Colors.white),
-                  //       ),
-                  //     ),
-                  //   ),
-                  // ),
-                  SliverAppBar(
-                      title: Text('aaa'),
-                      expandedHeight: 200,
-                      floating: false,
-                      pinned: true,
-                      flexibleSpace: FlexibleSpaceBar(
-                        collapseMode: CollapseMode.pin,
-                        background: Image.network(
-                          'https://placehold.co/600x200?text=KDRC+Header',
-                          fit: BoxFit.cover,
-                        ),)),
-                  ValueListenableBuilder<double>(
-
-                    valueListenable:
-                    scrollHeightNotifier,
-                    builder: (
-                        BuildContext context,
-                        double scrollHeight,
-                        Widget? child,
-                        ) {
-
-                      return SliverToNestedScrollBoxAdapter(
-                        childExtent: scrollHeight,
-                        onScrollOffsetChanged: (double scrollOffset) {
-                          double y = scrollOffset;
-                          if (Platform.isAndroid) {
-                            // https://github.com/flutter/flutter/issues/75841
-                            y *= window.devicePixelRatio;
-                          }
-                          webViewController
-                              .scrollTo(0, y.ceil());
-                        },
-                        child: child,
-                      );
-                    },
-                    child: WebViewWidget(controller: webViewController),
-                  ),
-                  SliverToBoxAdapter(
-                    child: Container(
-                      height: 300,
-                      color: Colors.green,
-                      child: const Center(
-                        child: Text(
-                          'Footer',
-                          style: TextStyle(color: Colors.white),
-                        ),
+                  child: WebViewWidget(controller: webViewController),
+                ),
+              ],
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.grey[50],
+            shape: const CircleBorder(),
+            child: Icon(
+              Icons.call,
+              color: Color.fromARGB(255, 247, 176, 116),
+              size: 36,
+            ),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      shape: RoundedRectangleBorder(),
+                      title: Text(
+                        'Приемная',
+                        style: TextStyle(fontWeight: FontWeight.bold),
                       ),
-                    ),
-                  ),
-
-                ],
-              ),
-            ],
-          )
+                      content: Text('Позвонить в приёмную?'),
+                      actions: [
+                        TextButton(
+                          child: Text('Да'),
+                          onPressed: () async{
+                            final Uri _url = Uri.parse('https://flutter.dev');
+                            await launchUrl(_url);
+                          },
+                        ),
+                        TextButton(
+                          child: Text('Нет'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        )
+                      ],
+                    );
+                  });
+            }),
       ),
-    )
-
-    ;
+    );
   }
-
 }
-
-
-
 
 const String scrollHeightJs = '''(function() {
   var height = 0;
@@ -191,7 +190,3 @@ const String scrollHeightJs = '''(function() {
     timer && clearTimeout(timer);
   };
 })();''';
-
-
-
-
